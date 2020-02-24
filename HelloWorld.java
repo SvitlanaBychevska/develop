8ae17bf04bb91fdd4c2c23522ba516ff94182be0
SvitlanaBychevska <Svitlana_Bychevska@epam.com>
2020-02-24 22:01:37 +0200

    Add new EQSolver file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,6 @@
 public class HelloWorld {
 public static void main(String []args) {
        System.out.println("Hello World"); // prints Hello World
-       System.out.println("Changes after First commit!");
-       System.out.println("Changes after Pushing to remote!");
+       System.out.println("Changes after First commit!")
     }
 } 
